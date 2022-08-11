12d239bdb9ef7be117f15103039426b84e87188c
principles31 <principles31@gmail.com>
2022-08-11 14:32:49 +0000

    Fix error

--- solver.rb
+++ solver.rb
@@ -1,29 +1,29 @@
 class Solver
   def factorial(num)
     return 'No negative integer. This is an exception.' if num.negative?
-  
+
     case num
     when 0
-        1
+      1
     else
-        num * factorial(num - 1)
+      num * factorial(num - 1)
     end
-    end
-  
-    def reverse(word)
-      word.reverse
-    end
-  
-    def fizzbuzz(number)
-      num = number.to_i
+  end
+
+  def reverse(word)
+    word.reverse
+  end
+
+  def fizzbuzz(number)
+    num = number.to_i
     if (num % 3).zero? && (num % 5).zero?
-        'fizzbuzz'
+      'fizzbuzz'
     elsif (num % 3).zero?
-        'fizz'
+      'fizz'
     elsif (num % 5).zero?
-        'buzz'
+      'buzz'
     else
-        num
+      num
     end
-    end
+  end
 end
